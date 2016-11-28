542ab957ae9a8565ea000397c4390d5fa217ac3d
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2016-11-28 18:10:11 -0500

    more tests

--- test/Test/LifeSpec.purs
+++ test/Test/LifeSpec.purs
@@ -27,7 +27,12 @@ spec = do
       downOrWrap z'' `shouldEqual` z
   let z1 = Zipper Nil 1 (List.singleton 2)
       z2 = Zipper Nil 3 (List.singleton 4)
+      z1' = Zipper (List.singleton 1) 2 Nil
+      z2' = Zipper (List.singleton 3) 4 Nil
       planeZ = Z (Zipper Nil z1 (List.singleton z2))
+      planeZ' = Z (Zipper (List.singleton z1) z2 Nil)
+      planeZr = Z (Zipper Nil z1' (List.singleton z2'))
+      planeZr' = Z (Zipper (List.singleton z1') z2' Nil)
       zArray = [[1,2], [3,4]]
   describe "fromFoldable" do
     it "converts a Foldable into a Z" do
@@ -35,3 +40,20 @@ spec = do
   describe "toUnfoldable" do
     it "converts a Z into an Unfoldable" do
       Life.toUnfoldable planeZ `shouldEqual` zArray
+  describe "zDown" do
+    it "moves the cursor down one" do
+      zDown planeZ `shouldEqual` Just planeZ'
+      zDown planeZr `shouldEqual` Just planeZr'
+  describe "zUp" do
+    it "moves the cursor up one" do
+      zUp planeZ' `shouldEqual` Just planeZ
+      zUp planeZr' `shouldEqual` Just planeZr
+  describe "zRight" do
+    it "moves the cursor right one" do
+      zRight planeZ `shouldEqual` Just planeZr
+      zRight planeZ' `shouldEqual` Just planeZr'
+  describe "zLeft" do
+    it "moves the cursor left one" do
+      zLeft planeZr `shouldEqual` Just planeZ
+      zLeft planeZr' `shouldEqual` Just planeZ'
+
