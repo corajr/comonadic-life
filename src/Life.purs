5a5384aedecf468596abb88002789baf4962f3d7
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2016-11-29 18:13:56 -0500

    out of bounds problem

--- src/Life.purs
+++ src/Life.purs
@@ -2,119 +2,114 @@ module Life where
 
 
 -- based on http://blog.emillon.org/posts/2012-10-18-comonadic-life.html
+-- and https://github.com/gelisam/conway/blob/master/src/Conway.hs
 
 import Prelude
-import Data.List.Zipper
-import Test.QuickCheck.Gen
-import Data.List as List
-import Data.List.Zipper as Zipper
+import Data.Array as Array
 import Control.Apply (lift2)
 import Control.Comonad (class Comonad, extract)
+import Control.Comonad.Trans.Class (class ComonadTrans)
 import Control.Extend (class Extend, extend)
-import Data.Array (filter, length)
+import Control.MonadZero (guard)
+import Data.Array (filter, length, toUnfoldable, (!!), (..))
 import Data.Bounded (class Bounded, bottom)
 import Data.Foldable (class Foldable)
+import Data.Generic (class Generic, gShow)
 import Data.List (List(..), reverse)
 import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Newtype (class Newtype, wrap, unwrap)
 import Data.String (joinWith, fromCharArray)
 import Data.Traversable (class Traversable, traverse)
 import Data.Tuple (Tuple(Tuple))
 import Data.Unfoldable (class Unfoldable, unfoldr, replicate)
-import Test.QuickCheck.Arbitrary (class Arbitrary, arbitrary, class Coarbitrary, coarbitrary)
+import Partial.Unsafe (unsafeCrashWith)
 
-data Z a = Z (Zipper (Zipper a))
+class Indexable t i where
+  getIndex :: forall a. t a -> i -> a
 
-instance functorZ :: Functor Z where
-    -- map :: forall a b. (a -> b) -> Z a -> Z b
-    map f (Z z) = Z (map (map f) z)
+infix 5 getIndex as !
 
-instance extendZ :: Extend Z where
-    -- extend :: forall b a. (Z a -> b) -> Z a -> Z b
-    extend f = map f <<< duplicate
-        where duplicate z = Z (map horizontal (vertical z))
-              horizontal = genericMove zLeft zRight
-              vertical = genericMove zUp zDown
-              genericMove :: forall a. (Z a -> Maybe (Z a)) -> (Z a -> Maybe (Z a)) -> Z a -> Zipper (Z a)
-              genericMove a b z = Zipper (maybeIterate a z) z (maybeIterate b z)
+data ArrayZipper a = ArrayZipper { array :: Array a
+                                 , index :: Int
+                                 }
 
-instance comonadZ :: Comonad Z where
-    -- extract :: forall a. Z a -> a
-    extract (Z z) = extract (extract z)
+derive instance eqArrayZipper :: (Eq a) => Eq (ArrayZipper a)
+derive instance ordArrayZipper :: (Ord a) => Ord (ArrayZipper a)
+derive instance genericArrayZipper :: (Generic a) => Generic (ArrayZipper a)
+instance showArrayZipper :: (Show a) => Show (ArrayZipper a) where
+  show (ArrayZipper { array, index }) = show array
 
-derive instance eqZ :: (Eq a) => Eq (Z a)
+getOrDie = fromMaybe (unsafeCrashWith "Out of bounds!")
 
--- this instance is invalid and exists only to support boundedZ
-instance ordZ :: (Ord a) => Ord (Z a) where
-  compare y z = compare y' z'
-    where y' :: Array (Array a)
-          y' = toUnfoldable y
-          z' :: Array (Array a)
-          z' = toUnfoldable z
+instance indexableArrayZipper :: Indexable ArrayZipper Int where
+  getIndex (ArrayZipper {array, index}) i = getOrDie (array !! index')
+    where index' = (index + 1) `mod` length array
 
-instance boundedZ :: (Bounded a) => Bounded (Z a) where
-  bottom = Z (Zipper Nil (Zipper Nil bottom Nil) Nil)
-  top =  Z (Zipper Nil (Zipper Nil top Nil) Nil)
+instance functorArrayZipper :: Functor ArrayZipper where
+    -- map :: forall a b. (a -> b) -> ArrayZipper a -> ArrayZipper b
+    map f (ArrayZipper z@{array, index}) = ArrayZipper (z { array = map f array })
 
-instance showZ :: (Show a) => Show (Z a) where
-    show (Z z) = show z
+instance extendArrayZipper :: Extend ArrayZipper where
+    -- extend :: forall b a. (ArrayZipper a -> b) -> ArrayZipper a -> ArrayZipper b
+    extend f z@(ArrayZipper z'@{array}) = ArrayZipper (z' { array = array' })
+      where array' = map (f <<< flip shift z) (0 .. (length array - 1))
 
-instance arbitraryZ :: (Arbitrary a, Bounded a) => Arbitrary (Z a) where
-  arbitrary = do
-    n <- chooseInt 0 3
-    xs <- vectorOf n (vectorOf n arbitrary)
-    pure (fromMaybe emptyZ (fromFoldable xs))
+instance comonadZ :: Comonad ArrayZipper where
+    -- extract :: forall a. ArrayZipper a -> a
+    extract z = z ! 0
 
-instance coarbitraryZ :: (Coarbitrary a) => Coarbitrary (Z a) where
-  coarbitrary xs = coarbitrary xs'
-    where xs' :: Array (Array a)
-          xs' = toUnfoldable xs
+shift :: forall a. Int -> ArrayZipper a -> ArrayZipper a
+shift i (ArrayZipper z@{ array, index }) = ArrayZipper (z { index = index' })
+    where index' = (index + 1) `mod` length array
 
--- | Moves cursor toward beginning, or wraps back around to end.
-upOrWrap :: forall a. Zipper a -> Zipper a
-upOrWrap z = case up z of
-  Just z' -> z'
-  Nothing -> end z
+newtype ArrayZipperT w a = ArrayZipperT (w (ArrayZipper a))
 
--- | Moves cursor toward end, or wraps back around to beginning.
-downOrWrap :: forall a. Zipper a -> Zipper a
-downOrWrap z = case down z of
-  Just z' -> z'
-  Nothing -> beginning z
+derive instance newtypeArrayZipperT :: Newtype (ArrayZipperT w a) _
 
--- | Move up (backward in the top-level Zipper).
-zUp :: forall a. Z a -> Maybe (Z a)
-zUp (Z z) = Z <$> up z
+shiftT :: forall a w. (Functor w) => Int -> ArrayZipperT w a -> ArrayZipperT w a
+shiftT i = wrap <<< map (shift i) <<< unwrap
 
--- | Move down (forward in the top-level Zipper).
-zDown :: forall a. Z a -> Maybe (Z a)
-zDown (Z z) = Z <$> down z
+instance indexableZipperT :: Indexable (ArrayZipperT ArrayZipper) (Tuple Int Int) where
+  getIndex z (Tuple x y) = extract (extract (shift y (unwrap (shiftT x z))))
 
--- | Move left (backward in all nested Zippers).
-zLeft :: forall a. Z a -> Maybe (Z a)
-zLeft (Z z) = Z <$> traverse up z
+instance functorArrayZipperT :: (Functor w) => Functor (ArrayZipperT w) where
+  map f = wrap <<< (map <<< map) f <<< unwrap
 
--- | Move left (backward in all nested Zippers).
-zRight :: forall a. Z a -> Maybe (Z a)
-zRight (Z z) = Z <$> traverse down z
+instance extendArrayZipperT :: (Comonad w) => Extend (ArrayZipperT w) where
+  extend f = ArrayZipperT <<< extend go <<< unwrap where
+    f' = f <<< wrap
+    go wz =
+      case extract wz of
+        ArrayZipper {array: xs, index: i } ->
+          let shifted_wzs = map (\j -> map (shift j) wz) (0 .. (length xs - 1))
+              ys = map f' shifted_wzs
+          in ArrayZipper {array: ys, index: i}
 
-maybeIterate :: forall a f. (Unfoldable f) => (a -> Maybe a) -> a -> f a
-maybeIterate f = unfoldr (map dup <<< f)
-  where dup a = Tuple a a
+instance comonadArrayZipperT :: (Comonad w) => Comonad (ArrayZipperT w) where
+  extract = extract <<< extract <<< unwrap
 
-neighbors :: forall a. Array (Z a -> Maybe (Z a))
-neighbors = horiz <> vert <> lift2 (>=>) horiz vert
-  where horiz = [zLeft, zRight]
-        vert = [zUp, zDown]
+instance comonadTransArrayZipperT :: ComonadTrans ArrayZipperT where
+  lower = map extract <<< unwrap
 
-emptyZ :: forall a. (Bounded a) => Z a
-emptyZ = Z (Zipper Nil (Zipper Nil bottom Nil) Nil)
+type Z a = ArrayZipperT ArrayZipper a
+
+derive instance eqZ :: (Eq a) => Eq (ArrayZipperT ArrayZipper a)
+
+instance showZ :: (Show a) => Show (ArrayZipperT ArrayZipper a) where
+  show = show <<< unwrap
+
+neighborIndices :: Array (Tuple Int Int)
+neighborIndices = do
+  x <- (-1) .. 1
+  y <- (-1) .. 1
+  let xy = Tuple x y
+  guard $ xy /= Tuple 0 0
+  pure xy
 
 -- | Boundaries are considered dead.
 aliveNeighbors :: Z Boolean -> Int
-aliveNeighbors z = length <<< filter id $ map fetch neighbors
-  where fetch dir = case dir z of
-          Just z' -> extract z'
-          Nothing -> false
+aliveNeighbors z = length <<< filter id $ map fetch neighborIndices
+  where fetch xy = z ! xy
 
 rule :: Z Boolean -> Boolean
 rule z =
@@ -127,12 +122,12 @@ evolve :: Z Boolean -> Z Boolean
 evolve = extend rule
 
 glider :: Z Boolean
-glider = fromMaybe emptyZ rs
-  where rs = fromFoldable ( replicate 3 fl <>
-                            [ fs <> [f, t, f] <> fs
-                            , fs <> [f, f, t] <> fs
-                            , fs <> [t, t, t] <> fs
-                            ] <> replicate 3 fl)
+glider = rs
+  where rs = fromArrays ( replicate 3 fl <>
+                         [ fs <> [f, t, f] <> fs
+                         , fs <> [f, f, t] <> fs
+                         , fs <> [t, t, t] <> fs
+                         ] <> replicate 3 fl)
         t = true
         f = false
         fs = replicate 6 f
@@ -140,17 +135,29 @@ glider = fromMaybe emptyZ rs
 
 
 disp :: Z Boolean -> String
-disp (Z z) = joinWith "\n" (map (fromCharArray <<< map f) z')
+disp z = joinWith "\n" (map (fromCharArray <<< map f) z')
   where z' :: Array (Array Boolean)
-        z' = toUnfoldable (Z z)
+        z' = toArrays z
         f :: Boolean -> Char
         f x = if x then '#' else ' '
 
+toUnfoldableZ :: forall a f. (Unfoldable f) => ArrayZipper a -> f a
+toUnfoldableZ (ArrayZipper {array}) = Array.toUnfoldable array
+
+fromArray :: forall a. Array a -> ArrayZipper a
+fromArray array = ArrayZipper { array, index: 0}
+
+toArray :: forall a. ArrayZipper a -> Array a
+toArray (ArrayZipper {array}) = array
+
+toArrays :: forall a. Z a -> Array (Array a)
+toArrays xs = map toArray (toArray (unwrap xs))
+
+fromArrays :: forall a. Array (Array a) -> Z a
+fromArrays xs = wrap (map fromArray (fromArray xs))
+
 toUnfoldable :: forall a f. (Unfoldable f) => Z a -> f (f a)
-toUnfoldable (Z z) = Zipper.toUnfoldable (map Zipper.toUnfoldable z)
+toUnfoldable = Array.toUnfoldable <<< map Array.toUnfoldable <<< toArrays
 
-fromList :: forall a. List (List a) -> Maybe (Z a)
-fromList x = Z <$> (traverse Zipper.fromFoldable x >>= Zipper.fromFoldable)
-
-fromFoldable :: forall a f. (Foldable f) => f (f a) -> Maybe (Z a)
-fromFoldable x = fromList (map List.fromFoldable (List.fromFoldable x))
+fromFoldable :: forall a f. (Functor f, Foldable f) => f (f a) -> Z a
+fromFoldable x = fromArrays (Array.fromFoldable (map Array.fromFoldable x))
--- test/Test/LifeSpec.purs
+++ test/Test/LifeSpec.purs
@@ -5,111 +5,67 @@ import Life
 import Data.List.Zipper
 import Data.List as List
 import Life as Life
-import Control.Comonad (extract)
-import Control.Extend (extend)
+import Control.Comonad (class Comonad, extract)
+import Control.Extend (class Extend, extend)
 import Data.Array (length, replicate)
 import Data.List (List(..))
 import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Newtype (class Newtype, wrap, unwrap)
 import Test.QuickCheck ((===))
+import Test.QuickCheck.Arbitrary (class Arbitrary, class Coarbitrary, arbitrary, coarbitrary)
+import Test.QuickCheck.Gen (chooseInt, vectorOf)
 import Test.Spec (Spec, describe, it, pending)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.QuickCheck (quickCheck)
 
+newtype ArbZ a = ArbZ (ArrayZipperT ArrayZipper a)
+
+instance arbArbZ :: (Arbitrary a) => Arbitrary (ArbZ a) where
+  arbitrary = do
+    n <- chooseInt 1 3
+    xs <- vectorOf n (vectorOf n arbitrary)
+    pure $ ArbZ (fromArrays xs)
+
+instance coarbArbZ :: (Coarbitrary a) => Coarbitrary (ArbZ a) where
+  coarbitrary (ArbZ a) = coarbitrary (toArrays a)
+
+instance showArbZ :: (Show a) => Show (ArbZ a) where
+  show (ArbZ z) = show z
+
+derive instance eqArbZ :: (Eq a) => Eq (ArbZ a)
+
+instance functorArbZ :: Functor ArbZ where
+  map f (ArbZ z) = ArbZ (map f z)
+
+instance extendArbZ :: Extend ArbZ where
+  extend f (ArbZ z) = ArbZ (extend (f <<< ArbZ) z)
+
+instance comonadArbZ :: Comonad ArbZ where
+  extract (ArbZ z) = extract z
+
 comonadLaws = do
   describe "Z Comonad laws" do
     it "extend extract = id" $
-      quickCheck \(xs :: Z Int) -> extend extract xs === xs
+      quickCheck \(xs :: ArbZ Int) -> extend extract xs === xs
     it "extract <<< extend f = f" $
-      quickCheck \(xs :: Z Int) (f :: Z Int -> Int) -> extract (extend f xs) === f xs
+      quickCheck \(xs :: ArbZ Int) (f :: ArbZ Int -> Int) -> extract (extend f xs) === f xs
     it "extend f <<< extend g = extend (f <<< extend g)" $
-      quickCheck \(xs :: Z Int) (f :: Z Int -> Boolean) (g :: Z Int -> Int) -> extend f (extend g xs) === extend (f <<< extend g) xs
-
-checkInverse a b =
-  quickCheck \(xs :: Z Boolean) -> (case a xs of
-                                       Just xs' -> b xs' === Just xs
-                                       Nothing -> xs === xs)
+      quickCheck \(xs :: ArbZ Int) (f :: ArbZ Int -> Boolean) (g :: ArbZ Int -> Int) -> extend f (extend g xs) === extend (f <<< extend g) xs
 
 -- spec :: forall r. Spec r Unit
 spec = do
-  let z = Zipper Nil 1 (List.fromFoldable [2, 3])
-      z' = Zipper (List.fromFoldable [1]) 2 (List.fromFoldable [3])
-      z'' = Zipper (List.fromFoldable [2, 1]) 3 Nil
-  describe "upOrWrap" do
-    it "moves a zipper up or wraps to end" $ do
-      upOrWrap z `shouldEqual` z''
-      upOrWrap z' `shouldEqual` z
-      upOrWrap z'' `shouldEqual` z'
-  describe "downOrWrap" do
-    it "moves a zipper down or wraps to beginning" $ do
-      downOrWrap z `shouldEqual` z'
-      downOrWrap z' `shouldEqual` z''
-      downOrWrap z'' `shouldEqual` z
-  let z1 = Zipper Nil 1 (List.singleton 2)
-      z2 = Zipper Nil 3 (List.singleton 4)
-      z1' = Zipper (List.singleton 1) 2 Nil
-      z2' = Zipper (List.singleton 3) 4 Nil
-      planeZ = Z (Zipper Nil z1 (List.singleton z2))
-      planeZ' = Z (Zipper (List.singleton z1) z2 Nil)
-      planeZr = Z (Zipper Nil z1' (List.singleton z2'))
-      planeZr' = Z (Zipper (List.singleton z1') z2' Nil)
-      zArray = [[1,2], [3,4]]
-      metaPlaneZ = [[planeZ, planeZr], [planeZ', planeZr']]
-      metaPlaneZ' = fromMaybe emptyZ (Life.fromFoldable metaPlaneZ)
-  describe "fromList" do
-    it "inverse of toUnfoldable" $
-      quickCheck \(xs :: Z Int) -> fromList (Life.toUnfoldable xs) === Just xs
-  describe "fromFoldable" do
-    it "converts a Foldable into a Z" do
-      Life.fromFoldable zArray `shouldEqual` Just planeZ
-  describe "toUnfoldable" do
-    it "converts a Z into an Unfoldable" do
-      Life.toUnfoldable planeZ `shouldEqual` zArray
-  describe "zDown" do
-    it "moves the cursor down one" do
-      zDown planeZ `shouldEqual` Just planeZ'
-      zDown planeZr `shouldEqual` Just planeZr'
-    it "inverts zUp" $ checkInverse zDown zUp
-  describe "zUp" do
-    it "moves the cursor up one" do
-      zUp planeZ' `shouldEqual` Just planeZ
-      zUp planeZr' `shouldEqual` Just planeZr
-    it "inverts zDown" $ checkInverse zUp zDown
-  describe "zRight" do
-    it "moves the cursor right one" do
-      zRight planeZ `shouldEqual` Just planeZr
-      zRight planeZ' `shouldEqual` Just planeZr'
-    it "inverts zLeft" $ checkInverse zLeft zRight
-  describe "zLeft" do
-    it "moves the cursor left one" do
-      zLeft planeZr `shouldEqual` Just planeZ
-      zLeft planeZr' `shouldEqual` Just planeZ'
-    it "inverts zRight" $ checkInverse zRight zLeft
-  describe "maybeIterate" do
-    it "iterates but discards the original input" do
-      maybeIterate (\x -> if x >= 8 then Nothing else Just (x*2)) 1 `shouldEqual` [2,4,8]
-  describe "extend" do
-    it "should turn a Z a into a Z (Z a)" do
-      extend id planeZ `shouldEqual` metaPlaneZ'
-    let get = fromMaybe emptyZ
-        arrayZ3 = replicate 3 (replicate 3 0)
-        planeZ3 = get (Life.fromFoldable arrayZ3)
-        metaArrayZ3 = [ [planeZ3, get (zRight planeZ3), get (zRight planeZ3 >>= zRight)]
-                      , [get (zDown planeZ3), get (zDown planeZ3 >>= zRight), get (zDown planeZ3 >>= zRight >>= zRight)]
-                      , [get (zDown planeZ3 >>= zDown), get (zDown planeZ3 >>= zDown >>= zRight), get (zDown planeZ3 >>= zDown >>= zRight >>= zRight)]
-                      ]
-        metaPlaneZ3 = Life.fromFoldable metaArrayZ3
-    it "should turn a 3x3 Z a into a Z (Z a)" do
-      Just (extend id planeZ3) `shouldEqual` metaPlaneZ3
-    it "should return the original plane when combined with extract" do
-      extend extract planeZ `shouldEqual` planeZ
+  describe "fromArrays" do
+    it "inverse of toArrays" $
+      quickCheck \(ArbZ xs :: ArbZ Int) -> let xs' = Life.toArrays xs
+                                           in fromArrays xs' === xs
   comonadLaws
   let sampleArray = [[false, false, false], [false, true, false], [false, false, false]]
       sampleArray' = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
-      sampleBoard = fromMaybe emptyZ (Life.fromFoldable sampleArray)
-      sampleBoard' = fromMaybe emptyZ (Life.fromFoldable sampleArray')
-  describe "neighbors" do
+      sampleBoard = fromArrays sampleArray
+      sampleBoard' = fromArrays sampleArray'
+  describe "neighborIndices" do
     it "refers to 8 directions" $
-      length neighbors `shouldEqual` 8
+      length neighborIndices `shouldEqual` 8
   describe "aliveNeighbors" do
     it "returns the number of alive neighbors" do
       extend aliveNeighbors sampleBoard `shouldEqual` sampleBoard'
